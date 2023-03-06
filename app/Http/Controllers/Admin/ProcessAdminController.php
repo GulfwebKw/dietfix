5653cd5f479d008ad83b3ca73ab233ec25d62a4e
Erfan Ebrahimi <persionhost@gmail.com>
2023-03-06 20:42:01 +0330

    add dashboard data

--- app/Http/Controllers/Admin/ProcessAdminController.php
+++ app/Http/Controllers/Admin/ProcessAdminController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 use App\Models\Admin\AdminMenu;
+use App\Models\Clinic\Payment;
 use App\Models\User;
 use App\Models\UserMeta;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
@@ -28,10 +30,123 @@ class ProcessAdminController extends AdminController
 
        // $this->beforeFilter('admin')->count();
 
-        $users = User::whereIn('role_id',[1,2,3,4])
-            ->count();
+        $data = \Cache::remember('admin_dashboard' , 15*60 , function (){
+            $users = User::whereIn('role_id',[1,2,3,4])
+                ->whereDate('created_at' , '>=' , Carbon::now()->subYear())
+                ->get();
+            $Count['register']['daily'] = $users->filter(function ($user){
+                return $user->created_at->isToday();
+            })->count();
+            $Count['register']['weekly'] = $users->filter(function ($user){
+                return $user->created_at->isAfter(Carbon::now()->subWeek());
+            })->count();
+            $Count['register']['monthly'] = $users->filter(function ($user){
+                return $user->created_at->isAfter(Carbon::now()->subMonth());
+            })->count();
+            $Count['register']['quarterly'] = $users->filter(function ($user){
+                return $user->created_at->isAfter(Carbon::now()->subQuarter());
+            })->count();
+            $Count['register']['yearly'] = $users->count();
 
-        return View::make('admin.home')->withUsers($users)->with('_pageTitle',trans('main.Home'));
+
+            $users = User::whereIn('role_id',[1,2,3,4])
+                ->whereDate('membership_start' , '>=' , Carbon::now()->subYear())
+                ->get();
+            $Count['membership']['daily'] = $users->filter(function ($user){
+                return Carbon::parse($user->membership_start)->isToday();
+            })->count();
+            $Count['membership']['weekly'] = $users->filter(function ($user){
+                return Carbon::parse($user->membership_start)->isAfter(Carbon::now()->subWeek());
+            })->count();
+            $Count['membership']['monthly'] = $users->filter(function ($user){
+                return Carbon::parse($user->membership_start)->isAfter(Carbon::now()->subMonth());
+            })->count();
+            $Count['membership']['quarterly'] = $users->filter(function ($user){
+                return Carbon::parse($user->membership_start)->isAfter(Carbon::now()->subQuarter());
+            })->count();
+            $Count['membership']['yearly'] = $users->count();
+
+
+            $users = User::whereIn('role_id',[1,2,3,4])
+                ->WhereNotNull('dob')
+                ->get();
+            $Count['birthday']['daily'] = $users->filter(function ($user){
+                return Carbon::parse($user->dob)->dayOfYear() == Carbon::now()->dayOfYear() ;
+            })->count();
+            $Count['birthday']['weekly'] = $users->filter(function ($user){
+                $dob = Carbon::parse($user->dob);
+                return Carbon::parse($user->dob)->addYears($dob->diffInYears(Carbon::now()))->isCurrentWeek();
+            })->count();
+            $Count['birthday']['monthly'] = $users->filter(function ($user){
+                $dob = Carbon::parse($user->dob);
+                return Carbon::parse($user->dob)->addYears($dob->diffInYears(Carbon::now()))->isCurrentMonth();
+            })->count();
+            $Count['birthday']['quarterly'] = $users->filter(function ($user){
+                $dob = Carbon::parse($user->dob);
+                return Carbon::parse($user->dob)->addYears($dob->diffInYears(Carbon::now()))->isCurrentQuarter();
+            })->count();
+            $Count['birthday']['yearly'] = $users->count();
+
+
+
+
+            $users = Payment::query()->where('paid' , 1 )->whereDate('created_at' , '>=' , Carbon::now()->subYear())->get();
+            $Count['payment']['daily'] = $users->filter(function ($user){
+                return $user->created_at->isToday();
+            })->sum('total');
+            $Count['payment']['weekly'] = $users->filter(function ($user){
+                return $user->created_at->isAfter(Carbon::now()->subWeek());
+            })->sum('total');
+            $Count['payment']['monthly'] = $users->filter(function ($user){
+                return $user->created_at->isAfter(Carbon::now()->subMonth());
+            })->sum('total');
+            $Count['payment']['quarterly'] = $users->filter(function ($user){
+                return $user->created_at->isAfter(Carbon::now()->subQuarter());
+            })->sum('total');
+            $Count['payment']['yearly'] = $users->sum('total');
+
+
+            $register = [];
+            $memberShip = [];
+            $payment = [];
+            foreach ( Carbon::now()->subDays(30)->daysUntil(Carbon::now()) as $days ){
+                $register[$days->toDateString()] = 0 ;
+                $memberShip[$days->toDateString()] = 0 ;
+                $payment[$days->toDateString()] = 0 ;
+            }
+            User::query()->whereIn('role_id',[1,2,3,4])
+                ->whereDate('created_at', '>=' , Carbon::now()->subDays(30))
+                ->get()
+                ->groupBy(function ($item) {
+                    return $item->created_at->toDateString();
+                })->map(function ($item) use (&$register) {
+                    $register[$item[0]->created_at->toDateString() ] = $item->count();
+                });
+            User::query()->whereIn('role_id',[1,2,3,4])
+                ->whereDate('membership_start', '>=' , Carbon::now()->subDays(30))
+                ->get()
+                ->groupBy(function ($item) {
+                    return Carbon::parse($item->membership_start)->toDateString();
+                })->map(function ($item) use (&$memberShip) {
+                    $memberShip[Carbon::parse($item[0]->membership_start)->toDateString() ] = $item->count();
+                });
+            Payment::query()->where('paid' , 1 )
+                ->whereDate('created_at', '>=' , Carbon::now()->subDays(30))
+                ->get()
+                ->groupBy(function ($item) {
+                    return $item->created_at->toDateString();
+                })->map(function ($item) use (&$payment) {
+                    $payment[$item[0]->created_at->toDateString() ] = $item->sum('total');
+                });
+            $Count['graph']['register'] = $register;
+            $Count['graph']['memberShip'] = $memberShip;
+            $Count['graph']['payment'] = $payment;
+
+            return $Count;
+        });
+
+        dd($data);
+        return View::make('admin.home' , ['animalistic_data' => $data])->with('_pageTitle',trans('main.Home'));
 
     }
     public function index()
